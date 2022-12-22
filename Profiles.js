00b281ac23bc8b52df996169e511c931f57a1f46
Lưu Văn Hoàng <120252781+Hoang-Lv@users.noreply.github.com>
2022-12-22 22:01:26 +0900

    change for call user's video
    change for call user's video

---
+++ Profiles.js
@@ -0,0 +1,69 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Consumer } from '~/Context';
+import classNames from 'classnames/bind';
+import styles from './Profiles.module.scss';
+import Header from './Header';
+import Container from './Container';
+import { GetUser } from '~/services';
+import ShowLoading from '~/components/ShowLoading';
+const cx = classNames.bind(styles);
+function Profiles() {
+    const context = Consumer();
+    const { user_ } = useParams();
+    let nickName = user_.slice(6);
+    const [token] = context.token;
+    const [isLogin] = context.isLogin;
+    const [authMe] = context.auth;
+    const [myProf, setMyProf] = useState(false);
+    const [data, setData] = useState({});
+    const [page, setPage] = useState(1);
+    const [showLoading, setShowLoading] = useState(true);
+    if (nickName.length == 0) nickName = JSON.parse(localStorage.getItem('nickname'));
+    const fetchApi = async () => {
+        setShowLoading(true);
+        const res = await GetUser(nickName, token);
+        setData(res);
+        setShowLoading(false);
+    };
+    useEffect(() => {
+        if (isLogin) {
+            fetchApi();
+            if (nickName === authMe.nickname) {
+                setData(authMe);
+                setMyProf(true);
+            }
+        }
+    }, [isLogin]);
+    useEffect(() => {
+        if (nickName === authMe.nickname) {
+            setData(authMe);
+            setMyProf(true);
+            setPage(1);
+        } else {
+            setData({});
+            fetchApi();
+            setMyProf(false);
+            setPage(1);
+        }
+    }, [nickName]);
+    return (
+        <div
+            className={cx('profile')}
+            onScroll={(e) => {
+                const list = e.target;
+                const listTop = list.offsetHeight;
+                const heightResult = list.children[1].clientHeight + list.children[0].clientHeight;
+                const result = heightResult - (listTop + list.scrollTop - 104);
+                if (result == 0) {
+                    setPage((prev) => prev + 1);
+                }
+            }}
+        >
+            <Header myProf={myProf} profile={data} />
+            <Container myProf={myProf} profile={data} page={page} setPage={setPage} />
+            {showLoading ? <ShowLoading /> : ''}
+        </div>
+    );
+}
+export default Profiles;
